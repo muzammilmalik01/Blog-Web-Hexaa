896d0d6b0713d97fb0e47226dda834a3fd82d425
Muhammad Muzamil <muzammil123malik@gmail.com>
2024-03-30 06:12:18 +0500

    Added Like Post, Comment Notifications - Generates a notification when user Likes a Post or a comment.

--- posts/consumers.py
+++ posts/consumers.py
@@ -23,3 +23,39 @@ class NotificationConsumer(AsyncWebsocketConsumer):
     async def send_notification(self, event):
         # Send notification to the client
         await self.send(text_data=event['text'])
+
+    async def like_notification(self, event):
+        post_id = event['post_id']
+        comment_id = event['comment_id']
+        liker_id = event['liker_id']
+        liker_username = event['liker_username']
+        recepient = event['recepient']
+        notification_type = event['notification_type']
+
+        if post_id is not None:
+            message = f'User {liker_username} liked post {post_id}. Recepient ID: {recepient}'
+            data = {
+                'post_id': post_id,
+                'comment_id':None,
+                'liker_id': liker_id,
+                'liker_username': liker_username,
+                'recipient': recepient,
+                'message': message,
+                'notification_type' : notification_type
+            }
+
+        else:
+            message = f'User {liker_username} liked comment {comment_id}. Recepient ID: {recepient}'
+            data = {
+                'post_id': post_id,
+                'comment_id':None,
+                'liker_id': liker_id,
+                'liker_username': liker_username,
+                'recipient': recepient,
+                'message': message,
+                'notification_type' : notification_type
+            }
+
+        json_data = json.dumps(data)
+
+        await self.send(text_data=json_data)
--- posts/signals.py
+++ posts/signals.py
@@ -3,7 +3,7 @@ from django.dispatch import receiver
 from .models import Post, PostHistory
 from channels.layers import get_channel_layer
 from asgiref.sync import async_to_sync
-from .models import Notifications
+from .models import Notifications, Like
 
 @receiver(post_save, sender=Post, dispatch_uid="create_post_history")
 def create_post_history(sender, instance, created, **kwargs):
@@ -50,3 +50,33 @@ def create_notification(user, message):
         'notifications',
         {'type': 'send_notification', 'text': 'New notification!'}
     )
+
+@receiver(post_save, sender = Like)
+def send_like_notification(sender, instance, created, **kwargs):
+    if created:
+        
+        channel_layer = get_channel_layer()
+        if instance.post is not None:
+            async_to_sync(channel_layer.group_send)(
+                'notifications',{
+                    'type' : 'like_notification',
+                    'post_id': instance.post.id,
+                    'comment_id' : None,
+                    'liker_id': instance.liked_by.id,
+                    'liker_username': instance.liked_by.username,
+                    'recepient' : instance.post.author.id,
+                    'notification_type' : 'add-post-like'
+                }
+            )
+        elif instance.post is None and instance.comment is not None:
+            async_to_sync(channel_layer.group_send)(
+                'notifications',{
+                    'type' : 'like_notification',
+                    'post_id': None,
+                    'comment_id' : instance.comment.id,
+                    'liker_id': instance.liked_by.id,
+                    'liker_username': instance.liked_by.username,
+                    'recepient' : instance.comment.author.id,
+                    'notification_type' : 'add-comment-like'
+                }
+            )
