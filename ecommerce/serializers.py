4d0fcd81b2cb642cce37dd9a11109ade9905d099
Muhammad Muzamil <muzammil123malik@gmail.com>
2024-04-23 14:16:26 +0500

    Fixed Serializer serializers.Serializer -> serializers.ModelSerializer

--- ecommerce/serializers.py
+++ ecommerce/serializers.py
@@ -2,31 +2,31 @@ from rest_framework import serializers
 from .models import Product_Category, Color, Product, Attribute, Image
 
 
-class Product_CategorySerializer(serializers.Serializer):
+class Product_CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Product_Category
         fields = "__all__"
 
 
-class ColorSerializer(serializers.Serializer):
+class ColorSerializer(serializers.ModelSerializer):
     class Meta:
         model = Color
         field = "__all__"
 
 
-class ProductSerializer(serializers.Serializer):
+class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
         field = "__all__"
 
 
-class AttributeSerializer(serializers.Serializer):
+class AttributeSerializer(serializers.ModelSerializer):
     class Meta:
         model = Attribute
         field = "__all__"
 
 
-class ImageSerializer(serializers.Serializer):
+class ImageSerializer(serializers.ModelSerializer):
     class Meta:
         model = Image
         field = "__all__"
